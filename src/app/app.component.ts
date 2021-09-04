16129916b20af3a5f0d0cecbde5d8c2564747022
vaibhavvijay9 <vaibhavvijay9@gmail.com>
2021-09-04 21:40:41 +0530

    scroll to list on slider change (mobile)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '@app/services/data.service';
 
@@ -11,6 +11,8 @@ export class AppComponent implements OnInit {
 
   productsList: any;
   filteredList: any;
+
+  @ViewChild('productsListRef') productsListRef;
 
   constructor(private dataService: DataService, public router: Router) {}
 
@@ -27,6 +29,9 @@ export class AppComponent implements OnInit {
 
   filterList(size) {
     this.filteredList = this.dataService.getFilteredList(size);
+
+    if(this.productsListRef)
+      this.productsListRef.nativeElement.scrollIntoView();  // scroll to list on Size slider change  (see effect on mobile)
   }
 
 
